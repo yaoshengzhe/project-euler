5575c8e03d485a20f2f18109fb769e7ba36f6890
Shengzhe Yao <syao@quantcast.com>
2012-08-15 18:59:35 -0700

    fix small bug in 71 (not affect final result)

--- problems/071.py
+++ problems/071.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python
 
+import math
 from util.prime import is_prime
 
 def gcd(x, y):
@@ -14,7 +15,7 @@ def main():
     candidate = (2, 5)
     target = 3.0/7
     for d in range(8, 1000001):
-        start, end = int(d*float(candidate[0])/candidate[1])+1, int(d*target)
+        start, end = int(d*float(candidate[0])/candidate[1])+1, int(math.ceil(d*target))
         for n in reversed(range(start, end)):
             if gcd(n, d) == 1:
                 candidate = (n, d)
